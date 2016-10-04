d367a54ed1e9bd7ceff25f2f29c0cbbfb909544c
Brian McGowan <mcgowan.b@gmail.com>
2016-10-04 22:49:56 +0100

    sample assigment completed

--- week-4/test-assignment/script.js
+++ week-4/test-assignment/script.js
@@ -8,3 +8,36 @@ function changeParagraph() {
     paragraph.style.transform = "rotate(-15deg)";
 
 }
+
+function getImageAttributes() {
+
+    var options = document.getElementsByName("attribute");
+    var num;
+
+    for (var i = 0; i < options.length; i++) {
+        if (options[i].checked) {
+            num = parseInt(options[i].value);
+            break;
+        }
+    }
+    displayAttributeValues(num);
+}
+
+function displayAttributeValues(num) {
+    var image = document.getElementById("image");
+
+    var attributeList = [];
+    attributeList[0] = "image src: " + image.getAttribute("src");
+    attributeList[1] = "image style: " + image.getAttribute("style");
+    attributeList[2] = "alt tag is: " + image.getAttribute("alt");
+    attributeList[3] = "image height attribute is: " + image.getAttribute("height");
+
+
+    var para = document.getElementById("att-list");
+    var currentText = para.innerHTML;
+    for (var i = 0; i < num; i++) {
+        currentText += "<br />" + attributeList[i];
+    }
+    para.innerHTML = currentText;
+
+}
