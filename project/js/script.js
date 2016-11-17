e3c228142c0e471befffa15b439d3998cb4681d2
Brian McGowan <mcgowan.b@gmail.com>
2016-11-17 18:16:26 +0000

    q1 finished

--- project/js/script.js
+++ project/js/script.js
@@ -31,15 +31,13 @@ function changeColours() {
     document.getElementById("strapline").setAttribute("style", "background: -webkit-linear-gradient(#F7B731, #676767); border-radius:0");
     document.querySelector("#menubar").setAttribute("style", "background: -webkit-linear-gradient(#F7B731, #676767); border-radius:0");
     document.getElementById("footer").setAttribute("style", "background: -webkit-linear-gradient(#F7B731, #676767); border-radius:0");
-    document.querySelector("#welcome span").style.color = "#fff";
-    // document.querySelector("#welcome h3").setAttribute("style", "text-shadow: 11px 3px 5px rgba(0, 0, 0, 1);");
 }
+
 function movePageContent() {
     document.getElementById("sidebar").style.float = "left";
     document.getElementById("content").style.float = "right";
     document.getElementById("slideshow").setAttribute("style", "float:right; margin-left:0; margin-right:20px");
 }
-
 
 function changeHeader() {
     var header = document.getElementById("header");
@@ -47,7 +45,6 @@ function changeHeader() {
 }
 
 function changeButtonStyles() {
-    //in here change class for pseudo selectors
     var actualButtons = document.querySelectorAll(".read-more");
     for (var i = 0; i < actualButtons.length; i++) {
         actualButtons[i].parentNode.className = "basic";
@@ -59,12 +56,8 @@ function changeButtonStyles() {
     for (var i = 0; i < menuButtons.length; i++) {
         menuButtons[i].style.fontFamily = "Bevan,cursive";
         menuButtons[i].style.paddingTop = "7px";
-        // var pn = menuButtons[i].parentNode;
-        // pn.setAttribute("class", "new-current");
-
     }
 }
-
 function changeFontType() {
     var tagList = ["h1", "h2", "h3"];
     tagList.forEach(changeFontTypeForElements);
@@ -72,11 +65,9 @@ function changeFontType() {
 
 function changeFontTypeForElements(item) {
     var headers = document.getElementsByTagName(item);
-    document.getElementsByTagName('p');
     for (var i = 0; i < headers.length; i++) {
         headers[i].style.fontFamily = "Bevan,cursive";
-        if(item == "h3"){
-            //targetting too many here
+        if (item == "h3") {
             headers[i].setAttribute("style", "text-shadow: 11px 6px 5px rgba(0, 0, 0, 1); font-family:Bevan, cursive")
         }
     }
@@ -91,7 +82,6 @@ function changeSliderImages() {
     var img = document.getElementById("slider");
     img.style.opacity = '0.0';
     img.style.borderRadius = "0";
-    // fadeOut("slider");
     img.setAttribute("src", "images/slider-" + imageCounter + ".jpg");
     fadeIn("slider");
     imageCounter++;
@@ -105,19 +95,6 @@ function fadeIn(id, val) {
     if (val < 9) {
         val++;
         setTimeout('fadeIn("' + id + '",' + val + ')', 90);
-    } else {
-        return;
-    }
-}
-
-function fadeOut(id, val) {
-    if (isNaN(val)) {
-        val = 9;
-    }
-    document.getElementById(id).style.opacity = '0.' + val;
-    if (val > 0) {
-        val--;
-        setTimeout('fadeOut("' + id + '",' + val + ')', 50);
     } else {
         return;
     }
